217105cf65fa2887ea25d3404f4542351fec8f48
chensteven <shchensteven@gmail.com>
2016-01-31 00:41:15 -0500

    first commit

---
+++ backup/send.php
@@ -0,0 +1,7 @@
+<?php 
+
+mail( "chensteven@outlook.com", "Signtrix - Potential Prospect Request",
+  "hello");
+header( "Location: index.html" );
+}
+?>
---
+++ email.php
@@ -0,0 +1,54 @@
+<?php 
+$errors = '';
+$myemail = 'tobias@signtrix.ca';//<-----Put Your email address here.
+if(empty($_POST['firstname'])  || 
+   empty($_POST['email_address']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: All fields are required";
+}
+
+$firstname = $_POST['firstname']; 
+$lastname = $_POST['lastname'];
+$companyname = $_POST['companyname'];
+$phone = $_POST['phone'];
+$email_address = $_POST['email_address']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "New Potential Prospect: $firstname $lastname";
+	$email_body = "You have received a new message. \n ".
+	" Here are the details:\n Name: $firstname $lastname \n Company: $companyname \n Phone: $phone \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: index.html#contact');
+} 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signtrix Contact Form Handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
